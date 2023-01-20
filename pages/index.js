7adb010dd7471d25909192447f3159f73125020f
scruz91 <pscn91@gmail.com>
2023-01-20 18:50:54 -0300

    se quita historial

--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,18 @@ import { IconButton } from '@mui/material';
 
 import CardMarket from '../components/cardMarket';
 import Layout, { siteTitle } from '../components/layout';
-import SearchHistory from '../components/searchHistory';
-
 
 export default function Home() {
 
   const [itemName, setItemName] = useState("");
-  const [results, setResults] = useState([]);
-  var history = [];
+  const [results, setResults] = useState([]);  
 
   const getItemByName = async () => {
 		try {
 			const res = await fetch(
 				`http://localhost:5000/api/market/${itemName}`
 			);
-			const data = await res.json();
-      history.push(itemName);
-      console.log(history)
+			const data = await res.json();      
       setResults(data)
 		} catch (err) {
 			console.log(err);
@@ -45,7 +40,6 @@ export default function Home() {
             </IconButton>
           </Grid>        
           <CardMarket items={results} />      
-          <SearchHistory history={history} />
       </Grid>      
     </Layout>
   );
