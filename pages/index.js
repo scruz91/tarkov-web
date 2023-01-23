fad7ece4e691d13950dfac254d498e9bafb601aa
scruz91 <pscn91@gmail.com>
2023-01-23 00:06:49 -0300

    feat: Loading + enter

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head';
 import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
-import SendIcon from '@mui/icons-material/Send';
-import { IconButton } from '@mui/material';
+import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
+import LoadingButton from '@mui/lab/LoadingButton';
+
 
 import CardMarket from '../components/cardMarket';
 import Layout, { siteTitle } from '../components/layout';
@@ -12,18 +13,28 @@ export default function Home() {
 
   const [itemName, setItemName] = useState("");
   const [results, setResults] = useState([]);  
+  const [loading, setLoading] = useState(false)
 
   const getItemByName = async () => {
 		try {
+      setLoading(true)
 			const res = await fetch(
 				`http://localhost:5000/api/market/${itemName}`
 			);
-			const data = await res.json();      
+			const data = await res.json();   
+      console.log(data)
+      setLoading(false)       
       setResults(data)
 		} catch (err) {
 			console.log(err);
 		}
 	};
+
+  const handleKeypress = e => {
+  if (e.keyCode === 13) {
+    getItemByName();
+  }
+};
 
   return (
     <Layout home>
@@ -32,12 +43,16 @@ export default function Home() {
       </Head>
       <Grid container spacing={2}>
           <Grid item xs={6}>
-            <TextField fullWidth size="small" id="outlined-basic" label="Item" variant="outlined" value={itemName} onChange={(e) => setItemName(e.target.value)}/>            
+            <TextField  onKeyUp={handleKeypress} fullWidth size="small" id="outlined-basic" variant="outlined" value={itemName} onChange={(e) => setItemName(e.target.value)}/>            
           </Grid>
           <Grid item xs={6}>
-            <IconButton aria-label="search" onClick={getItemByName}>
-              <SendIcon color="primary"/>
-            </IconButton>
+          <LoadingButton
+              loading={loading}
+              loadingPosition="start"
+              startIcon={<LocationSearchingIcon color="primary" />}
+              size="large"
+              onClick={getItemByName}
+            />
           </Grid>        
           <CardMarket items={results} />      
       </Grid>      
